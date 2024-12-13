3e18367b5aed9878dd5a6187747ea9c06915ad44
Aum-kar <1977omkarpatil@gmail.com>
2024-12-13 17:07:55 +0530

    Fixed issue of first_name, last_name not accepting the value from query parameter.

--- src/main/java/example/rest/controller/ApiController.java
+++ src/main/java/example/rest/controller/ApiController.java
@@ -4,7 +4,7 @@ import example.rest.model.User;
 import example.rest.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -17,12 +17,16 @@ public class ApiController {
 
     @GetMapping("all_users")
     public List<User> usersEndpoint() {
-        System.out.println("Call on /all_users endpoint");
         return service.getAllUser();
     }
 
     @GetMapping("add_user")
-    public void addUser(User new_user) {
-        service.addUser(new_user);
+    public void addUser(@RequestParam("uid") int uid,
+                        @RequestParam("first_name") String firstName,
+                        @RequestParam("last_name") String lastName,
+                        @RequestParam("address") String address,
+                        @RequestParam("skills") List<String> skills) {
+
+        service.addUser(new User(uid, firstName, lastName, address, skills));
     }
 }
--- src/main/java/example/rest/repository/UserRepository.java
+++ src/main/java/example/rest/repository/UserRepository.java
@@ -1,23 +1,22 @@
 package example.rest.repository;
 
 import example.rest.model.User;
+import lombok.Getter;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Getter
 @Repository
 public class UserRepository {
-    private List<User> users = new ArrayList<>();
+    private final List<User> users = new ArrayList<>();
+
 
     public UserRepository() {
         users.add(new User(1, "Omkar", "Patil", "Sangli", List.of("Java", "MySQL")));
         users.add(new User(2, "Sourabh", "Kothale", "Sangli", List.of("Python", "Cyber Security")));
         users.add(new User(3, "Shubham", "Patil", "Kasbe Digraj", List.of("MySQL", "Javascript")));
-    }
-
-    public List<User> getUsers() {
-        return this.users;
     }
 
     public void save(User new_user) {
